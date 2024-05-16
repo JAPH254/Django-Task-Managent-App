156efc540467562969eb8b3ec71e8a60f5efedb0
JAPH254 <omanyujapheth@gmail.com>
2024-05-16 12:51:56 +0300

    displayed data on the frontend

--- Task_Manager/urls.py
+++ Task_Manager/urls.py
@@ -25,4 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('server/', include('taskserver.urls')),
     path('users/', include('users.urls')),
+    
 ]
+# handle static files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
